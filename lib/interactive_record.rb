41b13a50410878047c61fa83cc5ab42e86bce08c
Kirsten Keels <keelsk@gmail.com>
2020-03-20 18:27:15 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,6 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
   end
