98aa0caf7fba7ef1df10427d23609f46b0f68858
Kirsten Keels <keelsk@gmail.com>
2020-03-20 18:24:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,8 @@ class InteractiveRecord
   end
   
   def self.find_by(options = {})
-    binding.pry
+    key = nil
+    value = nil
     options.each do |k, v|
       key = k.to_s
       value = v
