389db69a304038833ae0378f1e56d4eeaf72898f
Kirsten Keels <keelsk@gmail.com>
2020-03-20 16:49:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,6 +10,15 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].execute.results_as_hash = true
     
-    PRAGMA table_name
+    sql = "PRAGMA table_info ('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql)
+    
+    column_names = []
+    
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
   end
 end
